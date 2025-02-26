23bf9f310bf33bd47810568c006851ae0bd55cb9
Tien Do <tiendq@gmail.com>
2025-02-26 10:44:20 +0700

    fix: Restore Purchases button

--- Multiplatform/Account/RestorePurchasesButton.swift
+++ Multiplatform/Account/RestorePurchasesButton.swift
@@ -1,29 +1,34 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
+ See the LICENSE.txt file for this sample’s licensing information.
 
-Abstract:
-The restore purchases button.
-*/
+ Abstract:
+ The restore purchases button.
+ */
 
 import SwiftUI
 import StoreKit
 
 struct RestorePurchasesButton: View {
-    @State private var isRestoring = false
-    
-    var body: some View {
-        Button("Restore Purchases") {
-            isRestoring = true
-            Task.detached {
-                defer { isRestoring = false }
-                try await AppStore.sync()
-            }
+  @State private var isRestoring = false
+
+  var body: some View {
+    Button("Restore Purchases") {
+      isRestoring = true
+
+      Task.detached {
+        try? await AppStore.sync()
+
+        // Ref: https://stackoverflow.com/a/74493943/108616
+        await MainActor.run {
+          isRestoring = false
         }
-        .disabled(isRestoring)
+      }
     }
-    
+    .disabled(isRestoring)
+  }
+
 }
 
 #Preview {
-    RestorePurchasesButton()
+  RestorePurchasesButton()
 }
